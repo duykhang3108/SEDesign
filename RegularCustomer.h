7118840d7623854ca2f9ccab7606f549107945be
Anh NgHoang <51922754+s3759777@users.noreply.github.com>
2021-01-13 22:18:58 +0700

    Update RegularCustomer.h

--- RegularCustomer.h
+++ RegularCustomer.h
@@ -15,7 +15,7 @@ private:
     ItemLinkedList rentalList;
 
 public:
-    RegularCustomer(string id, string name, string address, string phone);
+    RegularCustomer(string id, string name, string address, string phone) : Customer(id, name, address, phone){};
 
     bool rentItem(Item item)
     {
